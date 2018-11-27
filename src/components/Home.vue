c33d28aa2aada5ac26dd216a85cc224549b72a31
Tyus Durant <contact@tyusdurant.com>
2018-11-27 16:31:45 -0500

    added from dt

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+        <h3>Home</h3>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Home",
+  mounted() {
+    console.log("working");
+  }
+};
+</script>
+
+<style>
+</style>
+
